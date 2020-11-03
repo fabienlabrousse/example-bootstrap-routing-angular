4fe55302370b5cbeec962a4c5e916ae6295abba1
fabienlabrousse <fabien.labrousse@la-combe-du-lion-vert.fr>
2020-11-03 09:53:27 +0100

    Ajout de tous les fichiers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FeatureComponent} from './feature/feature.component';
+import {PricingComponent} from './pricing/pricing.component';
+import {HomeComponent} from './home/home.component';
+import {FourOhFourComponent} from './four-oh-four/four-oh-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'features', component: FeatureComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeatureComponent } from './feature/feature.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HomeComponent } from './home/home.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { ThumbnailComponent } from './home/thumbnail/thumbnail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FeatureComponent,
+    PricingComponent,
+    HomeComponent,
+    FourOhFourComponent,
+    ThumbnailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
